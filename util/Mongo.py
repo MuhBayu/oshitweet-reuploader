dfdf99093a60e0ab68455e88ccdcedc8f21f15c8
Mochammad Bayu Nugraha <bnugraha00@gmail.com>
2020-03-28 19:25:33 +0700

    Update Mongo.py

--- util/Mongo.py
+++ util/Mongo.py
@@ -1,9 +1,7 @@
-from dotenv import load_dotenv
-load_dotenv()
 import os
 from pymongo import MongoClient
 # Connect to the MongoDB, change the connection string per your MongoDB environment
-client = MongoClient(os.getenv('MONNGO_URL'))
+client = MongoClient(os.getenv('MONGO_URL'))
 # Set the db object to point to the business database
 db = client['fiony_backups']
 media_collection = db['medias']
